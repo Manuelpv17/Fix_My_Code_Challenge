940fef572ef7f5d742741280aae7a36842b1d348
Manuelpv17 <manuelpv17@outlook.com>
2020-09-30 13:26:10 -0500

    size style

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,30 +4,28 @@
 
 class square():
     """ Class Square """
-
-    width = 0
-    height = 0
+    size = 0
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if kwargs:
+            self.size = kwargs["width"]
 
-    def areaOfMySquare(self):
+    def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.size * self.size
 
-    def permiterOfMySquare(self):
+    def PermiterOfMySquare(self):
         """ Perimeter """
-        return (self.width * 2) + (self.height * 2)
+        return (self.size * 2) + (self.size * 2)
 
     def __str__(self):
         """ Print """
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.size, self.size)
 
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.areaOfMySquare())
-    print(s.permiterOfMySquare())
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
