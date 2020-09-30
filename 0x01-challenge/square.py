2f8cae501c08c786940c4563f5ad9b25d107f29b
Manuelpv17 <manuelpv17@outlook.com>
2020-09-30 13:15:03 -0500

    more wow

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,7 +12,7 @@ class square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def areaOfMySquare(self):
         """ Area of the square """
         return self.width * self.width
 
@@ -29,5 +29,5 @@ if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
+    print(s.areaOfMySquare())
     print(s.permiterOfMySquare())
