a9e99115d25924a4cc97251ef59ed6e9da5f045e
Manuelpv17 <manuelpv17@outlook.com>
2020-09-30 18:09:05 -0500

    New area

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -38,7 +38,7 @@ class Square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """ Perimeter """
