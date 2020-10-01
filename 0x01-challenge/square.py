994ecab230a9e6ff572d137da0b0de271da2823c
Manuelpv17 <manuelpv17@outlook.com>
2020-09-30 23:33:47 -0500

    Remove setter and getter

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -12,36 +12,6 @@ class Square():
         self.width = width
         self.height = height
 
-    @property
-    def width(self):
-        """ width                    """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ width                    """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
-
-    @property
-    def height(self):
-        """ height                 """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ height                 """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
-
     def area_of_my_square(self):
         """ Area of the square                """
         return self.width * self.height
